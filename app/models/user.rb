ae679e6dc20d2869d4e394811d1aab085f555bd9
The Tiến <tiendt2311@gmail.com>
2017-04-08 09:16:35 +0700

    Set restriction without education email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,11 @@
 class User < ApplicationRecord
   def self.from_omniauth(auth)
-    where(google_uid: auth.uid).first_or_create do |user|
-      user.name = auth.info.name
-      user.avatar = auth.info.image
-      user.google_uid = auth.uid
+    if auth.info.email =~ /edu\.vn\z/
+      where(google_uid: auth.uid).first_or_create do |user|
+        user.name = auth.info.name
+        user.avatar = auth.info.image
+        user.google_uid = auth.uid
+      end
     end
   end
 end

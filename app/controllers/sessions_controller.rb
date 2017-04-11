ba23b286266e0f81f023ef305e7c1d86094bf4a7
Phong Phan <fongfan999@gmail.com>
2017-04-11 12:49:27 +0700

    Revert "Upload avatar"

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,11 +3,6 @@ class SessionsController < ApplicationController
     if user = User.from_omniauth(request.env["omniauth.auth"])
       log_in(user)
       flash[:notice] = "Login success"
-      if user.profile.created_at == user.profile.updated_at
-        flash[:notice] = "Login suscess! - Please modify your profile"
-        redirect_to edit_profile_path(username: user.profile.username)
-        return
-      end
     else
       flash[:alert] = "You have to login by .edu.vn email";
     end

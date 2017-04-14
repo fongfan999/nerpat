1cd66ab20e95faf23d36d2e4a31fa3c0371a575f
Phong Phan <fongfan999@gmail.com>
2017-04-14 08:57:52 +0700

    Refactored users controller and routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,13 @@
 class UsersController < ApplicationController
   before_action :authenticate_user
+  before_action :set_nerge, only: %i(send_add_nerge_request
+    accept_add_patron_request decline_add_patron_request)
+  before_action :set_patron, only: %i(accept_add_nerge_request
+    decline_add_nerge_request send_add_patron_request)
 
   rescue_from ActiveRecord::RecordNotFound, with: :not_available 
 
   def send_add_nerge_request
-    @nerge = current_user.available_nerges.find(params[:id])
     current_user.send_nerpat_request_to(@nerge, "muốn nhận bạn làm Nerge")
     
     flash[:notice] = 'Đã gửi yêu cầu tới Nerge'
@@ -12,7 +15,6 @@ class UsersController < ApplicationController
   end
 
   def accept_add_nerge_request
-    @patron = current_user.available_patrons.find(params[:id])
     current_user.accept_add_nerge_request_from(@patron)
 
     flash[:notice] = 'Nhận Patron thành công'
@@ -20,7 +22,6 @@ class UsersController < ApplicationController
   end
 
   def decline_add_nerge_request
-    @patron = current_user.available_patrons.find(params[:id])
     current_user.decline_add_nerge_request_from(@patron)
 
     flash[:alert] = 'Đã xoá yêu cầu'
@@ -28,7 +29,6 @@ class UsersController < ApplicationController
   end
 
   def send_add_patron_request
-    @patron = current_user.available_patrons.find(params[:id])
     current_user.send_nerpat_request_to(@patron, "muốn nhận bạn làm Patron")
     
     flash[:notice] = 'Đã gửi yêu cầu tới Patron'
@@ -36,7 +36,6 @@ class UsersController < ApplicationController
   end
 
   def accept_add_patron_request
-    @nerge = current_user.available_nerges.find(params[:id])
     current_user.accept_add_patron_request_from(@nerge)
 
     flash[:notice] = 'Nhận Nerge thành công'
@@ -44,7 +43,6 @@ class UsersController < ApplicationController
   end
 
   def decline_add_patron_request
-    @nerge = current_user.available_nerges.find(params[:id])
     current_user.decline_add_patron_request_from(@nerge)
 
     flash[:alert] = 'Đã xoá yêu cầu'
@@ -67,6 +65,14 @@ class UsersController < ApplicationController
   end
 
   private
+    def set_nerge
+      @nerge = current_user.available_nerges.find(params[:id])
+    end
+
+    def set_patron
+      @patron = current_user.available_patrons.find(params[:id])
+    end
+
     def not_available
       flash[:alert] = "Nerge/Patron này không thể nhận. Vui lòng thử lại."
       redirect_to root_path
--- config/routes.rb
+++ config/routes.rb
@@ -1,36 +1,29 @@
 Rails.application.routes.draw do
-
   root 'pages#index'
 
   get '/auth/google_oauth2/callback', to: 'sessions#create'
-  get '/auth/facebook/callback', to: 'profiles#connect_to_facebook'
-  delete 'sessions/destroy', as: :logout
-
   get 'auth/facebook/callback', to: 'profiles#connect_to_facebook'
   get 'disconnect/facebook', to: 'profiles#disconnect_to_facebook'
-
-  resources :users, only: [] do
-    member do
-      post :send_add_nerge_request
-      post :accept_add_nerge_request
-      delete :decline_add_nerge_request
-
-      post :send_add_patron_request
-      post :accept_add_patron_request
-      delete :decline_add_patron_request
-      
-      delete :remove_nerge
-    end
-
-    collection do
-      delete :remove_patron
-    end
-  end
+  delete 'sessions/destroy', as: :logout
 
   get '/:username', to: 'profiles#show', as: :profile   
   get '/:username/edit', to: 'profiles#edit', as: :edit_profile
   patch '/:username', to: 'profiles#update', as: :update_profile
 
-  resources :groups, only: [:show, :edit, :update]
+  resources :users, only: [] do
+    member do
+      post :send_add_nerge_request
+      patch :accept_add_nerge_request
+      delete :decline_add_nerge_request
 
+      post :send_add_patron_request
+      patch :accept_add_patron_request
+      delete :decline_add_patron_request
+    end
+
+    delete :remove_nerge, on: :member
+    delete :remove_patron, on: :collection
+  end
+
+  resources :groups, only: [:show, :edit, :update]
 end

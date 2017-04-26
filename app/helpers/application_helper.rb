a9c9300eedce09d01cff05b183002ffcd3d8b49a
The Tiến <tiendt2311@gmail.com>
2017-04-26 16:27:28 +0700

    Create time in words helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,13 @@ module ApplicationHelper
 
     "<i #{icon_class_attr}>#{icon_name}</i>".html_safe
   end
+
+  def time_in_words(time)
+    from_time = (Time.now - time)/1.day
+    if from_time >= 2
+      return (I18n.localize time, format: :long)
+    else
+      return "#{time_ago_in_words(time)} trước"
+    end
+  end
 end

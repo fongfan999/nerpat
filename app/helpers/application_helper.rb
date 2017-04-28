dc60e9f5c919ded09f140798d84aebafbda883c4
The Tiến <tiendt2311@gmail.com>
2017-04-28 23:38:12 +0700

    Change return of method display_time

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,7 +7,7 @@ module ApplicationHelper
 
   def display_time(time)
     if (Time.zone.now - time) > 1.day
-      return (I18n.localize time, format: :long)
+      return l(time, format: "%d %B, %Y lúc %H:%M")
     else
       "#{time_ago_in_words(time)} trước"
     end

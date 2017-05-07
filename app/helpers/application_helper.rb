b8a5900a38d389e305779ee9359d3ed827febbf1
Phong Phan <fongfan999@gmail.com>
2017-05-07 14:51:35 +0700

    Remove redundant style files and buttons

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,12 +2,12 @@ module ApplicationHelper
   def m_icon(icon_name, options = {})
     icon_class_attr = "class='material-icons #{options[:class]}'"
 
-    "<i #{icon_class_attr}>#{icon_name}</i>".html_safe
+    "<i #{icon_class_attr}>#{icon_name}</i> #{options[:text]}".html_safe
   end
 
   def display_time(time)
     if (Time.zone.now - time) > 1.day
-      return l(time, format: "%d %B, %Y lúc %H:%M")
+      l(time, format: "%d %B, %Y lúc %H:%M")
     else
       "#{time_ago_in_words(time)} trước"
     end

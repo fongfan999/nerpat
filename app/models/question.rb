640fb7298a8b500d4e51b16668ba4146b2ff7480
Phong Phan <fongfan999@gmail.com>
2017-05-08 17:21:35 +0700

    Display preloader inside main div

--- app/models/question.rb
+++ app/models/question.rb
@@ -4,7 +4,7 @@ class Question < ApplicationRecord
   has_many :answers, dependent: :destroy
   has_many :votes, as: :votable
 
-  validates :title, presence: true, length: { minimum: 5, maximum: 50 }
+  validates :title, presence: true, length: { minimum: 5, maximum: 150 }
   validates :body, presence: true, length: { minimum: 10 }
 
   def count_answers
